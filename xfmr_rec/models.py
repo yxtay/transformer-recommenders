c24223742fc8fd1678ba1a9a396bc5293bd5a14b
yxtay <5795122+yxtay@users.noreply.github.com>
2025-09-24 16:04:02 +0800

    Update models.py

--- xfmr_rec/models.py
+++ xfmr_rec/models.py
@@ -30,36 +30,29 @@ class ModelConfig(pydantic.BaseModel):
 
 
 def init_bert(config: ModelConfig) -> BertModel:
-    model = None
     tokenizer = None
     if None in (config.vocab_size, config.max_seq_length):
         tokenizer = AutoTokenizer.from_pretrained(  # nosec
             config.pretrained_model_name
         )
-
     if config.vocab_size is None:
         config.vocab_size = tokenizer.vocab_size
     if config.max_seq_length is None:
         config.max_seq_length = tokenizer.model_max_length
 
-    if None in (
-        config.hidden_size,
-        config.num_hidden_layers,
-        config.num_attention_heads,
-        config.intermediate_size,
-    ):
+    params = [
+        "hidden_size",
+        "num_hidden_layers",
+        "num_attention_heads",
+        "intermediate_size",
+    ]
+    if any(getattr(config, p) is None for p in params):
         model = AutoModel.from_pretrained(  # nosec
             config.pretrained_model_name
         )
-
-    if config.hidden_size is None:
-        config.hidden_size = model.config.hidden_size
-    if config.num_hidden_layers is None:
-        config.num_hidden_layers = model.config.num_hidden_layers
-    if config.num_attention_heads is None:
-        config.num_attention_heads = model.config.num_attention_heads
-    if config.intermediate_size is None:
-        config.intermediate_size = model.config.intermediate_size
+        for p in params:
+            if getattr(config, p) is None:
+                setattr(config, p, getattr(model.config, p))
 
     bert_config = BertConfig(
         vocab_size=config.vocab_size,
